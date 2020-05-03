0b1ee954c821c5f963e2cd8671fafe4b1cb733b1
Shuvrajyoti Paul <shuvro997@gmail.com>
2020-05-03 18:11:19 +0530

    added error hendling

--- src/App.js
+++ src/App.js
@@ -3,56 +3,65 @@ import SearchForm from './components/SearchForm';
 import InfoCards from './components/InfoCards'
 import Chart from './components/Chart';
 import './App.css';
-import FetchData from "./components/fetchData";
+import { FetchSummary, FetchCountryData } from "./components/fetchData";
 
 function App() {
-  const [seacrchInput, setSearchInput] = useState("Global")
-  const [info, setInfo] = useState({})
-  const [status, setStatus] = useState("ok")
+  const [searchInput, setSearchInput] = useState("Global")
+  const [info, setInfo] = useState([])
+  const [status, setStatus] = useState({ summaryData: 'ok', countryData: 'ok' })
   const [globalInfo, setGlobalInfo] = useState({})
-
-
-  const getData = async (seacrchInput) => {
-    //const keyword = seacrchInput === "Global" ? "" : seacrchInput
-    if (info === {})
-      console.log("empty")
-    const data = await FetchData({ seacrchInput, setStatus })
+  const [CountryWiseData, setCountryWiseData] = useState([])
+  const filterData = () => {
+    if (status.summaryData !== 'ok')
+      return null
+    const data = info.filter((d) => { return d.Country === searchInput })
+    return data[0]
+  }
+  const getCountryData = async () => {
+    const country = filterData()
+    const data = await FetchCountryData({ country, setStatus, status })
+    setCountryWiseData(data)
+  }
+  const getData = async () => {
+    const data = await FetchSummary({ setStatus, status })
     console.log(data)
-    if (seacrchInput === "Global") {
-      setGlobalInfo(data)
-    }
-    else
-      setInfo(data)
+    setGlobalInfo(data.Global)
+    setInfo(data.Countries)
+
   }
   // eslint-disable-next-line
-  useEffect(() => { getData(seacrchInput) }, [seacrchInput])
-
+  useEffect(() => { getData() }, [])
+  // eslint-disable-next-line
+  useEffect(() => { getCountryData() }, [searchInput])
 
   return (
     <div className="App">
       <div className={'countrySelector'}>
         <h1>GLOBAL COVID19 TRACKER</h1>
       </div>
-      {status === 'error' && seacrchInput === "Global" ? "Something went wrong :( " :
+      {status.summaryData === 'error' ? <p className="statusError">Something went wrong :( </p> :
         <>
           <InfoCards data={globalInfo} />
           <br></br>
-          <Chart data={globalInfo} />
-        </>
+          {/* <Chart data={globalInfo} /> */}
 
-      }
-      <br></br>
-      <div className={'countrySelector'}>
-        <h1>{seacrchInput === "" ? "GLOBAL" : seacrchInput.substring(10).toUpperCase()} COVID19 TRACKER</h1>
-        <SearchForm seacrchInput={seacrchInput} setSearchInput={setSearchInput} />
-      </div>
-      {status === 'error' && seacrchInput !== "Global" ? "Something went wrong :( Try again. Check if you searched a valid country name" :
-        <>
-          <InfoCards data={info} searchInput={seacrchInput} />
           <br></br>
-          <Chart data={info} searchInput={seacrchInput} />
+          <div className={'countrySelector'}>
+            <h1>{searchInput === "" || searchInput === "Global" ? "Get Country Info" : searchInput.toUpperCase() + " COVID19 INFO"} </h1>
+            <SearchForm searchInput={searchInput} setSearchInput={setSearchInput} data={info} />
+          </div>
+          <InfoCards data={(() => filterData())()} />
+          {status.countryData === 'error' ? <p className="statusError">Something went wrong :( Chart unavailable</p> :
+            <>
+
+              <br></br>
+              <Chart data={CountryWiseData} searchInput={searchInput} />
+            </>
+          }
         </>
       }
+
+
     </div>
   );
 }
